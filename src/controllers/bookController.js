e10798fa758d71d5adfba99e9bcda211990d9cdc
Alvaro Batista <alvarobatista87@outlook.com>
2023-08-29 19:50:05 -0300

    :hammer: Refactor: associating data between schemas

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -3,7 +3,7 @@ import books from "../models/Book.js";
 class BookController {
 
     static searchBooks = (req, res) => {
-        books.find((err, books) => {
+        books.find().populate('author').exec((err, books) => {
             res.status(200).json(books)
         })
     }
@@ -11,7 +11,7 @@ class BookController {
     static searchBookById = (req, res) => {
         const { id } = req.params
 
-        books.findById(id, (err, books) => {
+        books.findById(id).populate('author', 'name').exec((err, books) => {
             if(err) {
                 res.status(400).send({message: `${err.message} - Id do livro não localizado.`})
             } else {
